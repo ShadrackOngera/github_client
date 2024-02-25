01375a6b4da9da0d152e1f65adedfbd23e74fcc2
Shadrack Ongera <shadrack.ongera@ronforddigital.com>
2024-02-26 01:21:16 +0300

    client secret

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = '3d9824c62b3ba3d7d304';
+const githubClientSecret = '026fb960c6711b68de4066045598c781c5c6ed09';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
